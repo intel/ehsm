d186df0db8ece1edfecc6a803fa2f31aeea00796
Yang Huang <yang.huang@intel.com>
2021-11-25 13:55:36 +0800

    add dockerfile for the ehsm core service
    Signed-off-by: Yan, Shaopu <shaopu.yan@intel.com>

---
+++ docker/build_and_run.sh
@@ -0,0 +1,8 @@
+set -e
+docker build --build-arg https_proxy=$https_proxy \
+             --build-arg http_proxy=$http_proxy -f ./docker/Dockerfile -t ehsmcore:latest ./
+
+# Another container should expose AESM and its socket in aesmd-socket volume.
+# Replace /dev/sgx/enclave with /dev/isgx if you use the Legacy Launch Control driver
+docker run --env http_proxy --env https_proxyi --device=/dev/sgx/enclave -v aesmd-socket:/var/run/aesmd -it ehsmcore bash
+
