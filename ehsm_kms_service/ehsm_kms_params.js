dff04bf52ab3497957faf7b94170875f1ab0542d
Liang1XZhao <liang1x.zhao@intel.com>
2021-12-07 19:31:18 +0800

    add nodejs framework support for the ehsm_kms_service
    currently it can support the below REST API cmds:
    POST <ehsm_srv_address>/ehsm?Action=CreateKey
    POST <ehsm_srv_address>/ehsm?Action=Encrypt
    POST <ehsm_srv_address>/ehsm?Action=Decrypt
    POST <ehsm_srv_address>/ehsm?Action=GenerateDatakey
    
    test: passed the unit-test
    run the script: python test_aes.py

---
+++ ehsm_kms_service/ehsm_kms_params.js
@@ -0,0 +1,77 @@
+
+const ehsm_keyspec_t = {
+  EH_AES_GCM_128: 0,
+  EH_AES_GCM_256: 1,
+  EH_RSA_2048: 2,
+  EH_RSA_3072:3,
+  EH_EC_P256: 4,
+  EH_EC_P512: 5,
+  EH_EC_SM2: 6,
+  EH_SM4: 7,
+}
+const ehsm_keyorigin_t = {
+  EH_INTERNAL_KEY: 0,
+  EXTERNAL_KEY: 1,
+}
+
+const _6kb_length = 6 * 1024;
+
+const aad = {
+  type: 'string',
+  maxLength: 32,
+}
+const ehsm_kms_params = {
+  CreateKey: {
+    keyspec: {
+      type : 'const',
+      arr: Object.keys(ehsm_keyspec_t),
+      errortext: "The keyspec type is incorrect",
+    },
+    origin: {
+      type : 'const',
+      arr: Object.keys(ehsm_keyorigin_t),
+      errortext: "The origin type is incorrect",
+    }
+  },
+  Encrypt: {
+    cmk_base64: {
+      type: 'string',
+      maxLength: _6kb_length,
+      minLength: 1
+    },
+    plaintext: {
+      type: 'string',
+      maxLength: _6kb_length,
+      minLength: 1
+    },
+    aad,
+  },
+  Decrypt: {
+    cmk_base64: {
+      type: 'string',
+      maxLength: _6kb_length,
+      minLength: 1
+    },
+    ciphertext: {
+      type: 'string',
+      maxLength: _6kb_length,
+      minLength: 1
+    },
+    aad,
+  },
+  GenerateDataKey: {
+    cmk_base64: {
+      type: 'string',
+      maxLength: 1024,
+      minLength: 1
+    },
+    keylen: {
+      type: 'int',
+      maxNum: 1024,
+      minNum: 1
+    },
+    aad,
+  },
+ 
+}
+module.exports = { ehsm_kms_params, ehsm_keyspec_t, ehsm_keyorigin_t };
