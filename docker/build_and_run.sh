cb42105d14c4e5c450a386e6f02e72e5918ca975
Yan, Shaopu <shaopu.yan@intel.com>
2021-12-11 15:15:10 +0800

    refine the script for the docker
    Usage: build_and_run.sh <Commands>
    Commands:
    -b | build    Build the docker image for the ehsm_kms_service.
    -r | run      Run the docker image.
    -d | delete   Delete the container and the docker image.
    
    Signed-off-by: Yan, Shaopu <shaopu.yan@intel.com>

--- docker/build_and_run.sh
+++ docker/build_and_run.sh
@@ -1,8 +1,112 @@
-set -e
-docker build --build-arg https_proxy=$https_proxy \
-             --build-arg http_proxy=$http_proxy -f ./docker/Dockerfile -t ehsmcore:latest ./
+#!/usr/bin/env bash
 
-# Another container should expose AESM and its socket in aesmd-socket volume.
-# Replace /dev/sgx/enclave with /dev/isgx if you use the Legacy Launch Control driver
-docker run --env http_proxy --env https_proxy --device=/dev/sgx/enclave -v aesmd-socket:/var/run/aesmd -it -P ehsmcore
+set -e
+
+WORKDIR=$PWD
+EHSM_DOCKER_FILE_NAME="ehsm_kms_service.tar.gz"
+EHSM_DOCKER_IMAGE_NAME="ehsm_kms_service:latest"
+
+
+function build {
+	echo "[build] delete the old ehsm docker images and containers..."
+	delete
+
+	BUILD_ARGS="--rm --no-cache"
+	BUILD_ARGS="$BUILD_ARGS --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy"
+
+	echo "[build] create docker images..."
+	docker build $BUILD_ARGS -f $WORKDIR/docker/Dockerfile -t $EHSM_DOCKER_IMAGE_NAME $WORKDIR
+
+	echo "[build] save docker images..."
+	docker save $EHSM_DOCKER_IMAGE_NAME | gzip > $WORKDIR/$EHSM_DOCKER_FILE_NAME
+
+	docker rmi $EHSM_DOCKER_IMAGE_NAME 2>/dev/null
+
+    #docker images|grep none|awk '{print $3 }'|xargs docker rmi
+}
+
+function run {
+	USAGE=$(cat <<- EOM
+	Usage: run(-r) <ehsm_kms_service docker images>(optional)
+
+	run the docker image, if you don't add the file name then use the default one($EHSM_DOCKER_FILE_NAME).
+
+	EOM
+	)
+
+	echo "$USAGE"
+	while [ "$#" -gt 0  ]
+	do
+	case "$1" in
+		-h|--help)
+		echo "$USAGE"
+		exit
+		;;
+
+		*)
+		EHSM_DOCKER_FILE_NAME=($1)
+        shift
+        ;;
+	esac
+	done
+
+	docker load -i $EHSM_DOCKER_FILE_NAME
+
+	RUN_ARG="--env http_proxy=$http_proxy --env https_proxy=$https_proxy"
+	RUN_ARG="$RUN_ARG --device=/dev/sgx/enclave -v aesmd-socket:/var/run/aesmd"
+
+	# run the container
+	docker run -d $RUN_ARG -it -P $EHSM_DOCKER_IMAGE_NAME
+}
+
+function delete {
+	container_id=$(docker ps | grep -E $EHSM_DOCKER_IMAGE_NAME |awk '{print $1}')
+	if [ ! -z "$container_id" ]; then
+		echo "rm containers..."
+		docker rm -f $container_id
+	fi
+
+	img_id=$(docker images | grep -E $EHSM_DOCKER_IMAGE_NAME |awk '{print $3}')
+	if [ ! -z "$img_id" ]; then
+		echo "rm docker image..."
+		docker rmi -f $img_id
+	fi
+}
+
+
+USAGE=$(cat <<- EOM
+Usage: build_and_run.sh <Commands>
+Commands:
+  -b | build    Build the docker image for the ehsm_kms_service.
+  -r | run      Run the docker image.
+  -d | delete   Delete the container and the docker image.
+
+EOM
+)
+
+subcmd="$1"
+case $subcmd in
+	build | -b)
+		shift
+		build $@
+		;;
+
+	run | -r)
+		shift
+		run $@
+		;;
+
+	delete | -d)
+		shift
+		delete $@
+		;;
+
+	*)
+		echo "$USAGE"
+		exit
+		;;
+esac
+
+
+exit
 
