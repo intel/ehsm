5950241fa561f600e9006e16227d70aa2c9be29b
Liang1XZhao <Liang1XZhao@intel.com>
2022-01-17 18:32:49 +0800

    add k8s support for ehsm-kms
    test: passed the unittest
    
    Signed-off-by: Liang1XZhao <Liang1XZhao@intel.com>

---
+++ ehsm_kms_service/k8s/ehsm-destroy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+kubectl delete svc couchdb -n ehsm-kms
+kubectl delete svc ehsm-kms-service -n ehsm-kms
+kubectl delete deployment ehsm-kms-deployment -n ehsm-kms
+kubectl delete sts couchdb -n ehsm-kms
+kubectl delete pvc couch-persistent-storage-couchdb-0 -n ehsm-kms
+kubectl delete pv ehsm-pv-nfs -n ehsm-kms
+kubectl delete cm ehsm-configmap -n ehsm-kms
+kubectl delete secret ehsm-secret -n ehsm-kms
