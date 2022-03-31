0e4b7e04e2367b64bc12db888ff71052a644f596
Long Hanyu <hanyu.long@intel.com>
2022-03-31 15:06:58 +0800

    Add Key Management API tests in cli
    Add deletekey, enable/disablekey, listkey, delete all key, getversion tests in cli.
    
    Signed-off-by: Long Hanyu <hanyu.long@intel.com>

---
+++ test/cli/disablekey.py
@@ -0,0 +1,45 @@
+import requests
+import json
+import argparse
+import base64
+import time
+import random
+import hmac
+from hashlib import sha256
+from collections import OrderedDict
+import urllib.parse
+
+import _utils_
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', type=str, help='the address of the ehsm_kms_server', required=True)
+    parser.add_argument('--keyid', type=str, help='the keyid of symmetric cmk', required=True)
+    args = parser.parse_args()
+
+    base_url = args.url + "/ehsm?Action="
+    print(base_url)
+    return base_url, args.keyid
+
+def disablekey(base_url, keyid):
+    print('disable key with keyid %s' %(keyid))
+
+    payload = OrderedDict()
+    payload["keyid"] = keyid
+    params = _utils_.init_params(payload)
+    print('disablekey req:\n%s\n' %(params))
+
+    resp = requests.post(url=base_url + "DisableKey", data=json.dumps(params), headers=_utils_.headers)
+    if(_utils_.check_result(resp, 'DisableKey') == False):
+        return
+
+    print('disablekey resp:\n%s\n' %(resp.text))
+
+if __name__ == "__main__":
+    headers = _utils_.headers
+
+    base_url, keyid = get_args()
+
+    disablekey(base_url, keyid)
+
