3cfb4c4a5401767d16dd409cf526bab8daca553a
Tsz Lung Chung <tsz.lung.chung@intel.com>
2023-09-20 15:32:17 +0800

    feat(sdk/python): add remote attestation module for API and CLI

---
+++ sdk/python/ehsm/serializers/remote_attestation.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from .base import EHSMBase
+
+
+@dataclass
+class GenerateQuoteResponse(EHSMBase):
+    challenge: str
+    quote: str
+
+
+@dataclass
+class VerifyQuoteResponse(EHSMBase):
+    result: bool
+    nonce: str
+    mr_enclave: str
+    mr_signer: str
+    sign: str
+
+
+@dataclass
+class UploadQuotePolicy(EHSMBase):
+    policyId: str
+
+
+@dataclass
+class GetQuotePolicy(EHSMBase):
+    policyId: str
+    mr_enclave: str
+    mr_signer: str
