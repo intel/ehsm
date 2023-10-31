136fd822730f5946868c74a5c45341ecda09d936
Tsz Lung Chung <tsz.lung.chung@intel.com>
2023-10-31 09:34:22 +0800

    feat(sdk/python): add error handling for 4XX/5XX code in response

--- sdk/python/ehsm/exceptions.py
+++ sdk/python/ehsm/exceptions.py
@@ -1,2 +1,14 @@
 class CredentialMissingException(Exception):
     """Missing credentials (app_id or api_key) in a session"""
+
+
+class InvalidParamException(Exception):
+    """Invalid param when requesting to eHSM KMS service"""
+
+
+class ServerExceptionException(Exception):
+    """Server exception when requesting to eHSM KMS service"""
+
+
+class UnknownException(Exception):
+    """The repsonse contains a status code other than 200, 4XX and 5XX"""
