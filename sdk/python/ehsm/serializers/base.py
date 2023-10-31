b4c426b4298a7449fd1e7cd6bc20f2f1dc362430
Tsz Lung Chung <tsz.lung.chung@intel.com>
2023-10-31 09:34:22 +0800

    feat(sdk/python): add error handling for 4XX/5XX code in response

--- sdk/python/ehsm/serializers/base.py
+++ sdk/python/ehsm/serializers/base.py
@@ -1,6 +1,11 @@
 from typing import TypeVar, Generic
 from httpx import Response
 from pydantic.dataclasses import dataclass
+from ehsm.exceptions import (
+    InvalidParamException,
+    ServerExceptionException,
+    UnknownException,
+)
 
 T = TypeVar("T")
 U = TypeVar("U")
@@ -19,6 +24,18 @@ class EHSMBase:
         data = response.json()
         if "result" not in data:
             raise ValueError("Response does not have attribute 'result'")
+        if data["code"] >= 400 and data["code"] < 500:
+            raise InvalidParamException(
+                f"Response has status {data['code']}: {data['message']}"
+            )
+        if data["code"] >= 500:
+            raise ServerExceptionException(
+                f"Response has status {data['code']}: {data['message']}"
+            )
+        if data["code"] != 200:
+            raise UnknownException(
+                f"Response has status {data['code']}: {data['message']}"
+            )
         return EHSMResponse[cls](
             code=data["code"],
             message=data["message"],
