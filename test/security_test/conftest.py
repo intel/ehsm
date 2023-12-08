53588abc1fcb3aa09a06ea222dc0458c55821ec3
Tsz Lung Chung <tsz.lung.chung@intel.com>
2023-12-08 17:16:16 +0800

    refactor(test): remove legacy server test

--- test/security_test/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-def pytest_addoption(parser):
-    parser.addoption("--url", action="store", default="", type=str, help='the address of the ehsm_kms_server')#, required=True)
-
-@pytest.fixture
-def test_url(request):
-    return request.config.getoption("--url")
